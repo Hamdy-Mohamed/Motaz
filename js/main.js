aa4a640f958ba3dc63c0e0a77d53a53750367147
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-05-24 19:44:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-let icon = document.querySelector(".menu_icon");
-let links = document.getElementById('smallLinks')
-icon.addEventListener("click", () => {
-    icon.classList.toggle("clicked");
-});
-function res_nav() {
-    links.classList.toggle("small-links");
-    // links.classList.remove("d-none");
-}
-
-
-
+let icon=document.querySelector(".menu_icon"),links=document.getElementById("smallLinks");function res_nav(){links.classList.toggle("small-links")}icon.addEventListener("click",()=>{icon.classList.toggle("clicked")});
